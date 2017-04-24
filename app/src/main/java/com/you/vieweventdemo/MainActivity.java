30a6d186ba2cc643330defdfa8adc10ed22bef19
youxuan <1454739828@qq.com>
2017-04-24 12:00:30 +0800

    学习View的事件分发机制

--- app/src/main/java/com/you/vieweventdemo/MainActivity.java
+++ app/src/main/java/com/you/vieweventdemo/MainActivity.java
@@ -2,12 +2,38 @@ package com.you.vieweventdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG = "MyButton";
+    private MyButton mButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mButton = (MyButton) findViewById(R.id.myButton);
+
+        mButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                switch (event.getAction()){
+                    case MotionEvent.ACTION_DOWN:
+                        Log.d(TAG, "onTouch: Action_down");
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        Log.d(TAG, "onTouch: Action_move");
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        Log.d(TAG, "onTouch: Action_up");
+                        break;
+                }
+                return false;
+            }
+        });
     }
 }
